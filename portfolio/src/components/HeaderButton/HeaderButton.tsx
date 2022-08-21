1643ebc1ed85ff0f1b61b9f8a27d38366daee318
Jakub Sowa <jakub.sowa5@gmail.com>
2022-08-22 00:04:10 +0200

    feat: initial react app addition after a long break

---
+++ portfolio/src/components/HeaderButton/HeaderButton.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+
+interface Props {
+  mainText: string;
+}
+
+export default class HeaderButton extends React.Component<Props> {
+  render() {
+    return (
+      <button className="header-button">{this.props.mainText}</button>
+    );
+  }
+}
+
