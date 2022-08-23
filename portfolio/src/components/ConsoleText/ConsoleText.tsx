443bc4843934c99e0aab5721760e18bd5cd92a47
Jakub Sowa <jakub.sowa5@gmail.com>
2022-08-23 22:33:58 +0200

    feat: fully refactored console bit to work with react

--- portfolio/src/components/ConsoleText/ConsoleText.tsx
+++ portfolio/src/components/ConsoleText/ConsoleText.tsx
@@ -1,24 +1,41 @@
-﻿import React from 'react';
+﻿import React, {useEffect} from 'react';
 
 interface Props {
   mainText: string;
 }
 
-export default class ConsoleText extends React.Component<Props> {
-  textToDisplay = "";
-  i = 0;
+interface State {
+  textToDisplay: string,
+  underscoreVisible: boolean
+}
+
+export default class ConsoleText extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props);
+    this.state = {
+      textToDisplay: "",
+      underscoreVisible: true
+    }
+  }
+
+  componentDidMount() {
     this.setLetters();
+
+    setInterval(() => {
+      this.setState({
+        ...this.state,
+        underscoreVisible: !this.state.underscoreVisible
+      })
+    }, 400)
   }
 
   setLetters = () => {
     setTimeout(() => {
-      console.log("haha")
-      this.textToDisplay += this.props.mainText;
-      this.i++;
-      if (this.i < this.props.mainText.length) {
+      this.setState({
+        textToDisplay: this.props.mainText.slice(0, this.state.textToDisplay.length + 1),
+      });
+      if (this.state.textToDisplay.length < this.props.mainText.length) {
         this.setLetters();
       }
     }, 100)
@@ -27,8 +44,8 @@ export default class ConsoleText extends React.Component<Props> {
   render() {
     return (
       <>
-        <div id="console">{this.textToDisplay}</div>
-        <div id="console-underscore">_</div>
+        <div id="console">{this.state.textToDisplay}</div>
+        <div id="console-underscore" style={{visibility: this.state.underscoreVisible ? 'visible' : 'hidden'}}>_</div>
       </>
     );
   }
