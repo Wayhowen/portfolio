d1fc855edc13405d0b8008e830336ee94420f534
Jakub Sowa <jakub.sowa5@gmail.com>
2023-04-14 22:17:47 +0200

    added go back button, needs css twixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import "./index.css"
-import {useLocation, Routes, Route} from "react-router-dom";
+import {useLocation, Routes, Route, useNavigate} from "react-router-dom";
 import Home from "./pages/Home";
 import ErrorPage from "./pages/Error";
 import About from "./pages/About";
 
 export default function App() {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const [displayLocation, setDisplayLocation] = useState(location);
   const [transitionStage, setTransistionStage] = useState("animate-fadeIn");
@@ -26,6 +27,10 @@ export default function App() {
            }
          }}
     >
+      {
+        location.pathname !== "/" &&
+        <button className="fixed top-0 left-10 text-7xl text-white z-10 hover:scale-95 click:scale-95" onClick={() => navigate(-1)}>&lt;</button>
+      }
       <Routes location={displayLocation}>
         <Route path="/" element={<Home/>} errorElement={<ErrorPage/>}/>
         <Route path="/me" element={<About/>} errorElement={<ErrorPage/>}/>
