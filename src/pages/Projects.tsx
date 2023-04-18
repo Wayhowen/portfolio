7c54c652ca37ed0a17585e879b0d77ed9f04f0f4
Jakub Sowa <jakub.sowa5@gmail.com>
2023-04-18 22:07:09 +0200

    finished playing with refs

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,38 +1,40 @@
-import React, {MutableRefObject, useRef} from 'react';
+import React, {ForwardedRef, forwardRef, MutableRefObject, useRef} from 'react';
 import "../index.css"
 import {Project, ownProjects, workProjects, schoolProjects} from "../data/projectsData"
 
 export default function Projects() {
-  const schoolRef = useRef();
-  const workRef = useRef();
-  const ownRef = useRef(null);
+  const schoolRef: React.RefObject<HTMLDivElement> = React.createRef<HTMLDivElement>();
+  const workRef: React.RefObject<HTMLDivElement> = React.createRef<HTMLDivElement>();
+  const ownRef: React.RefObject<HTMLDivElement> = React.createRef<HTMLDivElement>();
 
   return (
     <div className="flex flex-col grow text-white gap-2">
       <div className="basis-1/2 flex flex-col p-4 gap-2 overflow-hidden">
         <div className="basis-1/6 grid grid-cols-3 font-bold gap-4">
-          <ProjectButton text="School Projects"/>
-          <ProjectButton text="Work Projects"/>
+          <ProjectButton text="School Projects" ref={schoolRef}/>
+          <ProjectButton text="Work Projects" ref={workRef}/>
           <ProjectButton text="Own Projects" ref={ownRef}/>
         </div>
         <div className="flex flex-row basis-5/6 justify-center sm:justify-self overflow-hidden">
           {/*Desktop view*/}
           <div
             className="basis-1/3 grid-cols-1 grid-rows-r grid-flow-row auto-rows-ar gap-y-4 overflow-scroll hidden sm:grid">
-            {schoolProjects.map(project => ProjectEntry(project))}
+            {schoolProjects.map(project => <ProjectEntry project={project}/>)}
           </div>
           <div
             className="basis-1/3 grid-cols-1 grid-rows-r grid-flow-row auto-rows-ar gap-y-4 overflow-scroll hidden sm:grid">
-            {workProjects.map(project => ProjectEntry(project))}
+            {workProjects.map(project => <ProjectEntry project={project}/>)}
           </div>
           <div
             className="basis-1/3 grid-cols-1 grid-rows-r grid-flow-row auto-rows-ar gap-y-4 overflow-scroll hidden sm:grid">
-            {ownProjects.map(project => ProjectEntry(project))}
+            {ownProjects.map(project => <ProjectEntry project={project}/>)}
           </div>
           {/*Mobile view*/}
           <div
             className="basis-full grid-cols-1 grid-rows-r grid-flow-row auto-rows-ar gap-y-4 overflow-scroll grid sm:hidden">
-            {[...schoolProjects, ...workProjects, ...ownProjects].map(project => ProjectEntry(project))}
+            {[...schoolProjects, ...workProjects, ...ownProjects].map(project => <ProjectEntry key={project.projectName} project={project} ref={
+              schoolProjects.includes(project) ? schoolRef : workProjects.includes(project) ? workRef : ownRef
+            }/>)}
           </div>
         </div>
       </div>
@@ -43,31 +45,35 @@ export default function Projects() {
 
 interface ButtonProps {
   text: string
-  ref?: MutableRefObject<null>
+  ref?: ForwardedRef<HTMLDivElement>
 }
 
-export function ProjectButton(buttonProps: ButtonProps) {
+const ProjectButton = forwardRef<HTMLDivElement, ButtonProps>((props, ref: ForwardedRef<HTMLDivElement>) => {
   const clickHandler = () => {
-    if (buttonProps.ref) {
+    if (ref) {
       // @ts-ignore
-      buttonProps.ref.current.scrollIntoView()
+      ref.current.scrollIntoView()
     }
   }
   return (
     <div className="flex justify-center items-center">
       <button className="border border-2 rounded-full p-3 group hover:translate-y-[-3px] hover:scale-95 hover:bg-indigo-500/25 click:scale-95
           transition-all duration-300" onClick={clickHandler}>
-        {buttonProps.text}
+        {props.text}
       </button>
     </div>
   )
+})
+
+interface ProjectEntryProps {
+  project: Project
 }
 
-const ProjectEntry = (project: Project) => {
+const ProjectEntry = forwardRef<HTMLDivElement, ProjectEntryProps>((props, ref: ForwardedRef<HTMLDivElement>) => {
   return (
-    <div className="flex flex-col border justify-center items-center mx-2">
-      <div className="basis-1/2">{project.projectName}</div>
+    <div ref={props.project.ref ? ref : null} className="flex flex-col border justify-center items-center mx-2">
+      <div className="basis-1/2">{props.project.projectName}</div>
       <div className="basis-1/2">Thumbnail</div>
     </div>
   );
-}
+})
