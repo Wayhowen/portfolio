79515bcbdd1e8e1f051fc0aa31bf9400054d2cf4
Jakub Sowa <jakub.sowa5@gmail.com>
2023-04-21 00:31:05 +0200

    styling to the display

--- src/components/ProjectDisplay/ProjectDisplay.tsx
+++ src/components/ProjectDisplay/ProjectDisplay.tsx
@@ -7,28 +7,29 @@ type Props = {
 export const ProjectDisplay = (props: Props) => {
   const project = props.project;
   return (
-    <div className="border-2 rounded-3xl basis-1/2 shrink-0 mx-4 flex flex-col ">
-      <div>
-        <DisplaySection displayText={project?.projectName}/>
+    <div className="border-2 rounded-3xl basis-1/2 shrink-0 mx-4 flex flex-col space-between">
+      <div className="basis-1/6 flex flex-row items-center border-b-2 rounded-xl">
+        <DisplaySection header="Name" displayText={project?.projectName}/>
       </div>
-      <div>
-        <DisplaySection displayText={project?.projectDescription}/>
+      <div className="basis-4/6 flex flex-row items-center">
+        <DisplaySection header="Description" displayText={project?.projectDescription}/>
       </div>
-      <div>
-        <DisplaySection displayText={project?.link}/>
+      <div className="basis-1/6 flex flex-row items-center">
+        <DisplaySection header="Links" displayText={project?.link}/>
       </div>
     </div>
   );
 };
 
 type DisplaySectionProps = {
+  header: string
   displayText: string | undefined
 }
 const DisplaySection = (props: DisplaySectionProps) => {
   return (
     <>
-      <div></div>
-      <div>{props.displayText}</div>
+      <div className="px-2 basis-1/6 font-bold">{props.header}:</div>
+      <div className="basis-5/6">{props.displayText}</div>
     </>
   )
 }
