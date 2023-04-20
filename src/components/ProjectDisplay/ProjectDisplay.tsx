dc5a6d5bf0b85d413ace764cd0cac84ff0fbd23a
Jakub Sowa <jakub.sowa5@gmail.com>
2023-04-21 00:11:51 +0200

    more code structure

--- src/components/ProjectDisplay/ProjectDisplay.tsx
+++ src/components/ProjectDisplay/ProjectDisplay.tsx
@@ -7,8 +7,28 @@ type Props = {
 export const ProjectDisplay = (props: Props) => {
   const project = props.project;
   return (
-    <div className="border basis-1/2 shrink-0 mx-4 ">
-      {project?.projectName}
+    <div className="border-2 rounded-3xl basis-1/2 shrink-0 mx-4 flex flex-col ">
+      <div>
+        <DisplaySection displayText={project?.projectName}/>
+      </div>
+      <div>
+        <DisplaySection displayText={project?.projectDescription}/>
+      </div>
+      <div>
+        <DisplaySection displayText={project?.link}/>
+      </div>
     </div>
   );
 };
+
+type DisplaySectionProps = {
+  displayText: string | undefined
+}
+const DisplaySection = (props: DisplaySectionProps) => {
+  return (
+    <>
+      <div></div>
+      <div>{props.displayText}</div>
+    </>
+  )
+}
--- src/data/projectsData.tsx
+++ src/data/projectsData.tsx
@@ -6,24 +6,24 @@ export interface Project {
 }
 
 export const schoolProjects: Project[] = [
-  { projectName: "Time Reversal", projectDescription: "Test description 3", ref: true },
-  { projectName: "VEP-api", projectDescription: "Test description 3" },
-  { projectName: "security-itu", projectDescription: "Test description 3" },
-  { projectName: "pizzacution-engine", projectDescription: "Test description 3" },
-  { projectName: "Effervesence", projectDescription: "Test description 3" },
+  { projectName: "Time Reversal", projectDescription: "Test description 3", link: "https://www.google.com", ref: true },
+  { projectName: "VEP-api", projectDescription: "Test description 3", link: "https://www.google.com" },
+  { projectName: "security-itu", projectDescription: "Test description 3", link: "https://www.google.com" },
+  { projectName: "pizzacution-engine", projectDescription: "Test description 3", link: "https://www.google.com" },
+  { projectName: "Effervesence", projectDescription: "Test description 3", link: "https://www.google.com" },
 ];
 
 export const workProjects: Project[] = [
-  { projectName: "Content Moderation Platform", projectDescription: "Test description 1", ref: true },
-  { projectName: "Clock Trace", projectDescription: "Test description 2" },
-  { projectName: "Personal Finance Manager", projectDescription: "Test description 3" },
-  { projectName: "Invoice Analyzer", projectDescription: "Test description 1" },
-  { projectName: "Enalyzer Surveys", projectDescription: "Test description 2" },
+  { projectName: "Content Moderation Platform", projectDescription: "Test description 1", link: "https://www.google.com", ref: true },
+  { projectName: "Clock Trace", projectDescription: "Test description 2", link: "https://www.google.com" },
+  { projectName: "Personal Finance Manager", projectDescription: "Test description 3", link: "https://www.google.com" },
+  { projectName: "Invoice Analyzer", projectDescription: "Test description 1", link: "https://www.google.com" },
+  { projectName: "Enalyzer Surveys", projectDescription: "Test description 2", link: "https://www.google.com" },
 ];
 
 export const ownProjects: Project[] = [
-  { projectName: "margin-bot", projectDescription: "Test description 1", ref: true },
-  { projectName: "daily-racoon", projectDescription: "Test description 2" },
-  { projectName: "advent-of-code", projectDescription: "Test description 2" },
-  { projectName: "f1_race_strategy", projectDescription: "Test description 2" },
+  { projectName: "margin-bot", projectDescription: "Test description 1", link: "https://www.google.com", ref: true },
+  { projectName: "daily-racoon", projectDescription: "Test description 2", link: "https://www.google.com" },
+  { projectName: "advent-of-code", projectDescription: "Test description 2", link: "https://www.google.com" },
+  { projectName: "f1_race_strategy", projectDescription: "Test description 2", link: "https://www.google.com" },
 ];
